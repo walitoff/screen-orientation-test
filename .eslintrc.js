81337ac628ec75e4c835827efe252a68611d82c2
Ramil Valitov <ramilvalitov@gmail.com>
2023-04-01 16:58:04 +0700

    [add] tests

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": false,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "globals": {
+        "UIkit": "readonly"
+    },
+    "plugins": [],
+    "settings": {},
+    "rules": {}
+}
