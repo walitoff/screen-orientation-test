18339c0d0c068a15ab1694a60082cde4578d877c
Ramil Valitov <ramilvalitov@gmail.com>
2024-11-29 21:42:16 +0300

    [add] update ESLint and packages

--- lighthouse-config.js
+++ lighthouse-config.js
@@ -1,9 +1,9 @@
 // see https://github.com/GoogleChrome/lighthouse/blob/master/docs/configuration.md
-module.exports = {
+export default {
     extends: 'lighthouse:default',
     settings: {
         // audits can be found here:
-        // https://github.com/GoogleChrome/lighthouse/blob/eba2a4d19c5786dc37e993858ff4b663181f81e5/lighthouse-core/config/default-config.js#L174
+        // https://github.com/GoogleChrome/lighthouse/blob/main/core/config/default-config.js
         skipAudits: [
             'is-on-https',
             'canonical', // for staging sites this will always be incorrect
